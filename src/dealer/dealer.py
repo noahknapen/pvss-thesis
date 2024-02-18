f1922f985db7e2fa76c67b1bc1e7f57a0599b3b0
Noah Knapen <noah.knapen@gmail.com>
2024-02-18 12:56:20 +0100

    Added untested implementation of the generation and encryption of the shares through Shamir secret sharing with the encryption in an ElGamal-like fashion

--- src/dealer/dealer.py
+++ src/dealer/dealer.py
@@ -8,4 +8,33 @@ class Dealer:
         self.generator = generator
         self.pub_keys = pub_keys
 
+    def share_stage(self, secret):
+        shares = self.__generate_encrypted_shares(secret)
+           
+    
+    #######################
+    ### PRIVATE METHODS ###
+    #######################
+ 
+    def __generate_encrypted_shares(self, secret):
+        shares = self.__generate_shares(secret)
+        self.__generate_polynomial(secret, shares)
+        encrypted_shares = self.__encrypt_shares(shares)
+        return encrypted_shares
+    
+    def __generate_shares(self, secret):
+        # Returns a list of n tuples containing the unique index (integer) and the share itself (16 bytes)
+        return Shamir.split(self.t, self.n, secret)
+        
+    def __encrypt_shares(self, shares):
+        # Returns a list of n tuples containing the unique index (integer) and the encrypted share (integer)
+        encrypted_shares = []
+        
+        for i in range(len(shares)):
+            encrypted_shares.append((shares[i][0], self.__encrypt_share(shares[i][1], self.pub_keys[i])))
 
+    def __encrypt_share(self, share, pub_key):
+        # Returns the encrypted share (integer)
+        pub_key_int = int.from_bytes(pub_key, "little")
+        share_int = int.from_bytes(share, "little")
+        return pow(pub_key_int, share_int) 
