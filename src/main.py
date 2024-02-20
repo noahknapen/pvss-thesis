adc09ce23ae9ed6c0c8ec619cbd0c8e43a249e8e
Noah Knapen <noah.knapen@gmail.com>
2024-02-20 20:56:40 +0100

    Added Cryptodome implementation of creating a generator

--- src/main.py
+++ src/main.py
@@ -1,8 +1,38 @@
-from cryptography.hazmat.primitives.asymmetric import dh
+from Cryptodome.Math.Primality import generate_probable_safe_prime
+from Cryptodome.Math.Numbers import Integer
+from Cryptodome.Random import get_random_bytes
+from party.party import Party
+
+def create_generator():
+    p = generate_probable_safe_prime(exact_bits=2048, randfunc=get_random_bytes)
+
+    while True:
+        g = pow(Integer.random_range(min_inclusive=2, 
+                                     max_inclusive=p, 
+                                     randfunc=get_random_bytes), 2, p)
+
+        if g in (1, 2):
+            continue
+
+        if (p-1) % g == 0:
+            continue
+
+        g_inv = g.inverse(p)
+        if (p-1) % g_inv == 0:
+            continue
+
+        return g
 
 def main():
-    pass
+    generator = create_generator()
+    party_list = []
+    pub_keys = []
+
+    for _ in range(5):
+        party_list.append(Party(generator, 2048))
+        pub_keys.append(party_list[-1].get_public_key())
+   
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
