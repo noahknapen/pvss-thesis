7df0b5c52f82228156097a28f391e57f5fc0ebc5
Noah Knapen <noah.knapen@gmail.com>
2024-02-27 19:03:31 +0100

    Fixed implementation errors in `dealer.py` regarding the proof `pi_pdl` and adapted the according test

--- test_dealer.py
+++ test_dealer.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import Mock
 from dealer import Dealer
 from Cryptodome.Protocol.SecretSharing import _Element
+from numpy.polynomial.polynomial import Polynomial 
 
 class TestDealer(unittest.TestCase):
     def test_initialization(self):
@@ -28,7 +29,7 @@ class TestDealer(unittest.TestCase):
         mock2.public_key = 5
         mock3 = Mock()
         mock3.public_key = 7
-        dealer = Dealer(3, 3, 5, 2, [mock, mock2, mock3]) # TODO: Need parties for this
+        dealer = Dealer(3, 3, 5, 2, [mock, mock2, mock3])
         coeffs = [_Element(1), _Element(2), _Element(3)]
         encrypted_shares = dealer.generate_encrypted_shares(coeffs)
 
@@ -36,6 +37,19 @@ class TestDealer(unittest.TestCase):
         for encrypted_share in encrypted_shares:
             self.assertIsInstance(encrypted_share[0], int)
             self.assertIsInstance(encrypted_share[1], _Element)
+    
+    def test_pi_pdl(self):
+        mock = Mock()
+        mock.public_key = 3
+        mock2 = Mock()
+        mock2.public_key = 5
+        mock3 = Mock()
+        mock3.public_key = 7
+        dealer = Dealer(3, 3, 5, 2, [mock, mock2, mock3])
+        (encrypted_r_x, z_x) = dealer.pi_pdl(Polynomial([1,2,3]), [(1, _Element(1)), (2, _Element(2)), (3, _Element(3))])
+
+        self.assertEqual(len(encrypted_r_x), 3)
+        self.assertEqual(z_x.degree(), 2)
         
 
 if __name__ == '__main__':
